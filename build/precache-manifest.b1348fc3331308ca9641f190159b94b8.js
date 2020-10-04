69e02dff714b966561dc30ed411ab9f8856fa50b
杨乐 <44945303+yangle-163@users.noreply.github.com>
2020-10-04 16:04:13 +0800

    react

---
+++ build/precache-manifest.b1348fc3331308ca9641f190159b94b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae63d131cdec997fde4fa9ddb788818c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "/static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ce9ea5b1f002a871fc",
+    "url": "/static/js/main.8d3165d6.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
